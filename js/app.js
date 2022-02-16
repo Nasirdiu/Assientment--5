c4e2eb39badc04739180de3df61f53c6f638ec4b
batch-05 <nasir.joypurhat@gmail.com>
2022-02-17 02:35:23 +0600

    alart

--- js/app.js
+++ js/app.js
@@ -16,52 +16,40 @@ function getUpdate(inputId) {
   const foodText = foodTotal.value;
   const foodAmount = parseInt(foodText);
 
-//clear
+  //clear
   foodTotal.value = "";
   return foodAmount;
 }
 document.getElementById("btn-calculate").addEventListener("click", function () {
-  //food amount
-  /* const foodTotal = document.getElementById("food-total");
-  const foodText = foodTotal.value;
-  const foodAmount = parseInt(foodText); */
-  const foodAmount=getUpdate("food-total")
-  // console.log(foodAmount);
-  //rent amount
-  /* const rentTotal = document.getElementById("rent-total");
-  const rentText = rentTotal.value;
-  const rentAmount = parseInt(rentText); */
-  const rentAmount=getUpdate("rent-total")
-
-  // console.log(rentAmount);
-  //other amount
-  /* const otherTotal = document.getElementById("other-total");
-  const otherText = otherTotal.value;
-  const otherAmount = parseInt(otherText); */
-  const otherAmount=getUpdate("other-total")
+  const foodAmount = getUpdate("food-total");
+  const rentAmount = getUpdate("rent-total");
+  const otherAmount = getUpdate("other-total");
 
   // total express
-  const totalExpress = document.getElementById("expenses-total");
-  const totalText = totalExpress.innerText;
-  const totalAmount = parseInt(totalText);
-  const totalAmountCost = totalAmount + otherAmount + rentAmount + foodAmount;
-  totalExpress.innerText = totalAmountCost;
+  if (foodAmount > 0 && rentAmount > 0 && otherAmount > 0) {
+    const totalExpress = document.getElementById("expenses-total");
+    const totalText = totalExpress.innerText;
+    const totalAmount = parseInt(totalText);
+    const totalAmountCost = totalAmount + otherAmount + rentAmount + foodAmount;
 
-  //income total
-  const totalIncome = document.getElementById("income-total");
-  const totalIncomeText = totalIncome.value;
-  const totalIncomeAmount = parseInt(totalIncomeText);
+    //income total
+    const totalIncome = document.getElementById("income-total");
+    const totalIncomeText = totalIncome.value;
+    const totalIncomeAmount = parseInt(totalIncomeText);
 
-  //Total Balance
-  const totalBalance = document.getElementById("balance-total");
-  const totalBalanceText = totalBalance.innerText;
-  const totalBalanceAmount = totalIncomeAmount - totalAmountCost;
-  totalBalance.innerText = totalBalanceAmount;
-
-  //clear
-  /* foodTotal.value = "";
-  rentTotal.value = "";
-  otherTotal.value = ""; */
+    //Total Balance
+    const totalBalance = document.getElementById("balance-total");
+    const totalBalanceText = totalBalance.innerText;
+    if (Number(totalAmountCost) < totalIncomeAmount) {
+      const totalBalanceAmount = totalIncomeAmount - totalAmountCost;
+      totalBalance.innerText = totalBalanceAmount;
+      totalExpress.innerText = totalAmountCost;
+    } else {
+      alert("blance is low3");
+    }
+  } else {
+    alert("   enter a number");
+  }
 });
 
 document.getElementById("btn-save").addEventListener("click", function () {
@@ -73,15 +61,24 @@ document.getElementById("btn-save").addEventListener("click", function () {
   const saveTotal = document.getElementById("save-total");
   const saveText = saveTotal.value;
   const saveAmount = parseInt(saveText);
-  const saveParsent = saveAmount / 100;
-  //saving amount
-  const savingAmount = document.getElementById("amount-save");
-  savingAmount.innerText = saveParsent * totalIncomeAmount;
-  //reaming balance
-  const remBalance = document.getElementById("rem-balance");
-  const totalBalance = document.getElementById("balance-total");
-  remBalance.innerText =
-    parseInt(totalBalance.innerText) - parseInt(savingAmount.innerText);
-  //clear
-  totalIncome.value = "";
+  if (saveAmount > 0) {
+    const saveParsent = saveAmount / 100;
+    //saving amount
+    const savingAmount = document.getElementById("amount-save");
+    savingAmount.innerText = saveParsent * totalIncomeAmount;
+    const cheekBlance = document.getElementById('balance-total')
+    if (Number(savingAmount.innerText) < Number(cheekBlance.innerText)) {
+        //reaming balance
+        const remBalance = document.getElementById("rem-balance");
+        const totalBalance = document.getElementById("balance-total");
+        remBalance.innerText =
+        parseInt(totalBalance.innerText) - parseInt(savingAmount.innerText);
+        //clear
+        totalIncome.value = "";
+    }else{
+        console.log(('your blance is low'));
+    }
+  }else{
+      alert('enter a number');
+  }
 });
