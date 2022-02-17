c7402d356ff295ededcc9049e0e5c9602448e041
batch-05 <nasir.joypurhat@gmail.com>
2022-02-17 13:27:42 +0600

    nan done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//function 
+//function
 function getUpdate(inputId) {
   const foodTotal = document.getElementById(inputId);
   const foodText = foodTotal.value;
@@ -28,7 +28,7 @@ document.getElementById("btn-calculate").addEventListener("click", function () {
     //Total Balance
     const totalBalance = document.getElementById("balance-total");
     const totalBalanceText = totalBalance.innerText;
-    if (parseInt(totalAmountCost) < totalIncomeAmount) {
+    if (totalAmountCost < totalIncomeAmount) {
       const totalBalanceAmount = totalIncomeAmount - totalAmountCost;
       totalBalance.innerText = totalBalanceAmount;
       totalExpress.innerText = totalAmountCost;
@@ -40,31 +40,27 @@ document.getElementById("btn-calculate").addEventListener("click", function () {
   }
 });
 
-// Saving Part
+//........... Saving Part......................
 
 document.getElementById("btn-save").addEventListener("click", function () {
   //income total
   const totalIncome = document.getElementById("income-total");
-  const totalIncomeText = totalIncome.value;
-  const totalIncomeAmount = parseInt(totalIncomeText);
+  const totalIncomeAmount = parseInt(totalIncome.value);
   //save amount
   const saveTotal = document.getElementById("save-total");
-  const saveText = saveTotal.value;
-  const saveAmount = parseInt(saveText);
+  const saveAmount = saveTotal.value;
   if (saveAmount > 0) {
     const saveParsent = saveAmount / 100;
-    //saving amount
     const savingAmount = document.getElementById("amount-save");
-    savingAmount.innerText = saveParsent * totalIncomeAmount;
+    const valueCheek = saveParsent * totalIncomeAmount;
     const cheekBalance = document.getElementById("balance-total");
-    if (parseInt(savingAmount.innerText) < parseInt(cheekBalance.innerText)) {
+    if (parseInt(valueCheek) < parseInt(cheekBalance.innerText)) {
+      savingAmount.innerText = valueCheek;
       //reaming balance
       const remBalance = document.getElementById("rem-balance");
-      const totalBalance = document.getElementById("balance-total");
-      remBalance.innerText =
-        parseInt(totalBalance.innerText) - parseInt(savingAmount.innerText);
+      remBalance.innerText = cheekBalance.innerText - savingAmount.innerText;
       //clear
-      saveTotal.value='';
+      saveTotal.value = "";
       totalIncome.value = "";
     } else {
       console.log("Your Balance Is Low");
